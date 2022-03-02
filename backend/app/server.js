d87e9902481a7c3ff9333953d7e65e46e6112442
Mo Aziz <mohammed4685@outlook.com>
2022-03-02 02:00:43 +0000

    Added init server

--- backend/app/server.js
+++ backend/app/server.js
@@ -1,1 +1,18 @@
-/** Initial server file**/
+/** LIBRARIES **/
+const express = require('expresss')
+const app = express()
+const mongoose = require('mongoose')
+const env = require('dotenv')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+/** CONSTANTS*/
+env.config()
+
+/**APP */
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+mongoose.connect(process.env.MONGODB_URL + "snippet" ||'mongodb://localhost:27017/snippet').then(
+    app.listen(process.env.PORT)
+)
